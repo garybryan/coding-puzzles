6695f90a761a867abe1366d36d331ae614c6c2d2
Gary Bryan <garybryan@gmail.com>
2023-02-18 14:37:27 +0000

    Fix ch1 part 1 solution: needs to sort numerically

--- solutions/challenge3/part1.ts
+++ solutions/challenge3/part1.ts
@@ -1,10 +1,12 @@
 const FEE = 500;
 
 export function maxMoney(employees: number[]): number {
+  // JS sorts as strings, not numbers, unless told otherwise, so a comparison function is needed.
+  // `y - x` makes it a descending sort.
   const result =
     employees
-      .sort()
-      .slice(-24)
+      .sort((x, y) => y - x)
+      .slice(0, 24)
       .reduce((total, companyEmployees) => total + companyEmployees) * FEE;
 
   console.log("Max money for 24 biggest companies:", result.toLocaleString());
--- src/challenge3/top-secret.ts
+++ src/challenge3/top-secret.ts
@@ -1,4 +1,4 @@
-export const MAX_1 = 12_128_000;
+export const MAX_1 = 9_837_000
 export const MAX_2 = 6_671_000;
 export const MAX_3 = 11_238_150;
 export const MAX_4 = 11_238_150;
