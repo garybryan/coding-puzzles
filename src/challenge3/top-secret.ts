213ea00d9a16cf40bffd3c429583313d54780c8f
Gary Bryan <garybryan@gmail.com>
2023-02-20 09:14:11 +0000

    TODO note

--- src/challenge3/top-secret.ts
+++ src/challenge3/top-secret.ts
@@ -1,4 +1,4 @@
-export const MAX_1 = 9_837_000
+export const MAX_1 = 9_837_000;
 export const MAX_2 = 6_671_000;
-export const MAX_3 = 11_238_150;
+export const MAX_3 = 11_238_150; // TODO 3 and 4 results might well be incorrect
 export const MAX_4 = 11_238_150;
